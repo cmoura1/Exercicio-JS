2ed1cd7876acd18eb84c8c3db712e9b672acf11f
cmoura1 <carolinacornelio@hotmail.com>
2022-02-21 22:09:17 -0300

    21-02-2022

--- js/jogo.js
+++ js/jogo.js
@@ -4,17 +4,21 @@ const jogo = {
     gols1: 0,
     gols2: 0,
     resultado: "",
-    atualizarPlacar: function (t1, t2) {
-        this.gols1 = parseInt(t1);
-        this.gols2 = parseInt(t2);
+    atualizaPlacar: function(g1 = 0, g2 = 0) {
+        this.gols1 = parseInt(g1);
+        this.gols2 = parseInt(g2);
     },
-    atualizaResultado: function (){
-        if(this.gol1 > this.gols2) {
-            this.resultado = this.time;
-        } else if (this.gol1 == this.gols2) {
-            this.resultado = "Empate"
+    atualizaResultado: function() {
+        if (this.gols1 > this.gols2) {
+            this.resultado = this.time1;
+        } else if (this.gols1 == this.gols2) {
+            this.resultado = "Empate";
         } else {
             this.resultado = this.time2;
         }
-    }
+    },
+    atualizaTimes: function(t1 = "", t2 = "") {
+        this.time1 = t1;
+        this.time2 = t2;
+    },
 }
